a5923cd55863ad23090f3a1205ac0d55bcbcabb7
shubhams.biencaps <shubhams.biencaps@gmail.com>
2021-04-08 12:37:25 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -25,14 +25,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Shubham Sangale'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Shubham Sangale'
     })
 })
 
@@ -40,7 +40,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Shubham Sangale'
     })
 })
 
@@ -98,7 +98,7 @@ app.get('/weather', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Shubham Sangale',
         errorMessage: 'Help article not found.'
     })
 })
@@ -106,7 +106,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Shubham Sangale',
         errorMessage: 'Page not found.'
     })
 })
